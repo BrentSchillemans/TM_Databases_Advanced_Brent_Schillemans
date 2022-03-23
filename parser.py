50b2ffd6982b54147f7ddb329b101616d58e9bb5
BrentSchillemans <brent.schillemans@hotmail.com>
2022-03-23 17:50:34 +0100

    Parser en scraper apart - werking via docker

---
+++ parser.py
@@ -0,0 +1,48 @@
+import requests, json
+from time import sleep
+from bs4 import BeautifulSoup
+from os.path import exists
+from pymongo import MongoClient
+import urllib.parse
+import redis
+
+## mongoDB
+client = MongoClient("mongodb://127.0.0.1:27017")
+db = client.cryptoscraper
+collection = db.transaction
+
+## Redis
+r = redis.Redis(
+    host="localhost",
+    port=8081
+)
+
+maxHash = ""
+maxValue = 0
+
+while True:
+
+    ## data ophalen uit Redis
+    for key in r.keys():
+        key = str(key).split("'")[1]
+        value = r.hgetall(key)
+        value = str(value).split("'")
+
+        hash = value[3]
+        time = value[7]
+        btc = value[11]
+        usd = value[15]
+        usdValue = float(usd.replace("$","").replace(",","",-1))
+
+        if usdValue > maxValue:
+            maxHash = hash
+            maxValue = usdValue
+
+        print(key,"-",time,"-",btc,"-",usd)
+        print()
+
+    print("Grootste hash:",maxHash,"met waarde $",maxValue)
+    print("Programma uitgevoerd!")
+
+    #Loop elke minuut
+    sleep(60)
--- scraper.py
+++ scraper.py
@@ -2,23 +2,14 @@ import requests, json
 from time import sleep
 from bs4 import BeautifulSoup
 from os.path import exists
-from pymongo import MongoClient
 import urllib.parse
 import redis
-
-## mongoDB
-username = urllib.parse.quote_plus('superuser')
-password = urllib.parse.quote_plus('p@ss')
-
-client = MongoClient("mongodb://%s:%s@127.0.0.1:27017" % (username,password))
-db = client.cryptoscraper
-collection = db.transaction
+from datetime import timedelta
 
 ## Redis
 r = redis.Redis(
-    host = 'localhost',
-    port = 6379,
-    password = 'p@ss'
+    host="localhost",
+    port=8081
 )
 
 while True:
@@ -42,16 +33,12 @@ while True:
             amount_btc = item.split("Amount (BTC)")[1].split("Amount (USD)")[0]
             amount_usd = item.split("Amount (BTC)")[1].split("Amount (USD)")[1]
 
-            ## checken in cache
-            if r.get(hash) == None:
-                ## nieuwe transacties wegschrijven naar DB
-                newData = {hash:{"Time":time,"Amount (BTC)":amount_btc,"Amount (USD)":amount_usd}}
-                collection.insert_one(newData)
-                print(hash)
+            newData = {"Hash":hash,"Time":time,"Amount (BTC)":amount_btc,"Amount (USD)":amount_usd}
+            print(hash)
 
-            ## transacties toevoegen aan cache en bijhouden voor 65 seconden (wordt na minuut bijgevoegd, maar met marge indien scraper vertraagd is)
-            r.set(hash, time, ex=65)
-
+            ## transacties toevoegen aan cache en bijhouden voor 60 seconden
+            r.hmset(hash, newData)
+            r.expire(hash, timedelta(seconds=60))
 
     print("Programma uitgevoerd!")
 
